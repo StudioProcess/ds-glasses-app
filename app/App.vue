02159b0e319969ebd3d4daacef926561f5cdb933
marie <dvorzak.marie@gmx.at>
2019-11-06 15:21:56 +0100

    swiper hover

--- app/App.vue
+++ app/App.vue
@@ -5,11 +5,11 @@
     <Glasses v-on:sendMessage="model = $event"></Glasses>
       <div class="material-picker-container">
 
-    <Materials index="1"></Materials>
-    <Materials index="2"></Materials>
-    <Materials index="3"></Materials>
-    <Materials index="4"></Materials>
-    <Materials index="5"></Materials>
+    <Materials index="1" swiperClass='Swiper1'></Materials>
+    <Materials index="2" swiperClass='Swiper2'></Materials>
+    <Materials index="3" swiperClass='Swiper3'></Materials>
+    <Materials index="4" swiperClass='Swiper4'></Materials>
+    <Materials index="5" swiperClass='Swiper5'></Materials>
     </div>
   </div>
 
@@ -24,6 +24,11 @@ export default {
   data: function() {
     return{
     model:"N°1 - Moluptatum",
+    Swiper1: null,
+    Swiper2: null,
+    Swiper3: null,
+    Swiper4: null,
+    Swiper5: null,
      msg: "",
     }
     },
--- app/src/components/MaterialPicker.vue
+++ app/src/components/MaterialPicker.vue
@@ -14,26 +14,33 @@
         v-on:click="setSliderContent('fabrics')"
         v-bind:class="[activeElement === 'fabrics' ? 'active text-tab' : 'text-tab']"
       >Stoff</li>
-      <span class="swiper swiper-wood swiper-container">
+      <span :class="'swiper swiper-wood swiper-container swiper-container'+index">
         <div class="swiper-wrapper">
           <span
             v-if="activeElement === 'woods'"
             v-for="wood in woods"
-            class="wood text-product-description swiper-slide"
+            class="text-product-description swiper-slide"
           >
             <h3 v-bind:style="{ backgroundImage: 'url(' + wood.texture + ')' }" class="bg"></h3>
           </span>
           <span
             v-if="activeElement === 'fabrics'"
             v-for="fabric in fabrics"
-            class="wood text-product-description swiper-slide"
+            class="text-product-description swiper-slide"
           >
             <h3 v-bind:style="{ backgroundImage: 'url(' + fabric.texture + ')' }" class="bg"></h3>
           </span>
+          <span
+            v-if="activeElement === 'papers'"
+            v-for="paper in papers"
+            class="wood text-product-description swiper-slide"
+          >
+            <h3 v-bind:style="{ backgroundImage: 'url(' + paper.texture + ')' }" class="bg"></h3>
+          </span>
         </div>
       </span>
-      <span class="swiper-button-prev"></span>
-      <span class="swiper-button-next"></span>
+      <span :class="'swiper-button-prev swiper-button-prev'+(index) "></span>
+      <span :class="'swiper-button-next swiper-button-next'+(index) "></span>
     </ul>
   </div>
 </template>
@@ -42,20 +49,41 @@
 // import Glasses from './components/GlassesPicker'
 import images from "../assets/materials/*/*.png";
 
-var mySwiper;
-
 export default {
   name: "MaterialPicker",
   components: {},
-  props: ["selectedModel", "index"],
+  props: ["selectedModel", "index", "swiperClass"],
   data: function() {
     return {
       isSelected: false,
       activeElement: "woods",
+      mSwiperClass: this.swiperClass,
       images,
       hoveredItem: 0,
       currentIndex: 0,
       fabrics: [
+        {
+          name: "Jeans",
+          texture: images["fabric"]["jeans1"]
+        },
+        {
+          name: "Jeans2",
+          texture: images["fabric"]["jeans2"]
+        },
+        {
+          name: "Jeans3",
+          texture: images["fabric"]["jeans3"]
+        },
+        {
+          name: "Jeans4",
+          texture: images["fabric"]["jeans4"]
+        },
+        {
+          name: "Jeans5",
+          texture: images["fabric"]["jeans5"]
+        }
+      ],
+      papers: [
         {
           name: "Jeans",
           texture: images["fabric"]["jeans1"]
@@ -112,7 +140,7 @@ export default {
         },
         {
           name: "Lärche",
-          texture: images["wood"]["lärche"]
+          texture: images["wood"]["laerche"]
         },
         {
           name: "Nuss",
@@ -127,54 +155,53 @@ export default {
   },
   mounted: function() {
     setTimeout(() => {
-    mySwiper = new Swiper(".swiper-container", {
-      slidesPerView: 7,
-      spaceBetween: 6,
-      slidesPerGroup: 7,
-      loop: false,
-      loopFillGroupWithBlank: false,
-      watchOverflow: true,
-      // Navigation arrows
-      navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev"
-      }
-    });
-    },20);
+      this.mSwiperClass = new Swiper(".swiper-container" + this.index, {
+        slidesPerView: 7,
+        spaceBetween: 6,
+        slidesPerGroup: 7,
+        loop: false,
+        loopFillGroupWithBlank: false,
+        watchOverflow: true,
+        navigation: {
+          nextEl: ".swiper-button-next" + this.index,
+          prevEl: ".swiper-button-prev" + this.index
+        }
+      });
+    }, 100);
+    setTimeout(() => {
+      this.mSwiperClass.update();
+    }, 300);
   },
   methods: {
     setSliderContent: function(material) {
       this.activeElement = material;
-      console.log("setSlider:" + material);
-    setTimeout(() => {
-       if (material === "fabrics") {
-        mySwiper.allowSlideNext = false;
-        mySwiper.allowSlidePrev = false;
-        mySwiper.paginationHide = true;
-        mySwiper.slidesPerView = 5;
-        document.getElementsByClassName(
-          "swiper-button-next"
-        )[0].style.visibility = "hidden";
-        document.getElementsByClassName(
-          "swiper-button-prev"
-        )[0].style.visibility = "hidden";
-        console.log(mySwiper[0]);
-        mySwiper[this.index-1].update();
-      }
-      if (material === "woods") {
-        mySwiper.allowSlideNext = true;
-        mySwiper.allowSlidePrev = true;
-        mySwiper.paginationHide = false;
-        document.getElementsByClassName(
-          "swiper-button-next"
-        )[0].style.visibility = "visible";
-        document.getElementsByClassName(
-          "swiper-button-prev"
-        )[0].style.visibility = "visible";
-        mySwiper[this.index-1].update();
-      }
-    }, 10);
-     
+      setTimeout(() => {
+        if (material === "fabrics" || material === "papers") {
+          this.mSwiperClass.allowSlideNext = false;
+          this.mSwiperClass.allowSlidePrev = false;
+          this.mSwiperClass.paginationHide = true;
+          this.mSwiperClass.slidesPerView = 5;
+          document.getElementsByClassName(
+            "swiper-button-next"
+          )[0].style.visibility = "hidden";
+          document.getElementsByClassName(
+            "swiper-button-prev"
+          )[0].style.visibility = "hidden";
+          this.mSwiperClass.update();
+        }
+        if (material === "woods") {
+          this.mSwiperClass.allowSlideNext = true;
+          this.mSwiperClass.allowSlidePrev = true;
+          this.mSwiperClass.paginationHide = false;
+          document.getElementsByClassName(
+            "swiper-button-next" + this.index
+          )[0].style.visibility = "visible";
+          document.getElementsByClassName(
+            "swiper-button-prev" + this.index
+          )[0].style.visibility = "visible";
+          this.mSwiperClass.update();
+        }
+      }, 10);
     }
   }
 };
